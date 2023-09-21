0588b8eca1aaf866e53522b949c98ae8f5cadd7f
Lucas Marcos <lucas_190201lmms@hotmail.com>
2023-09-20 22:13:47 -0300

    Criado tela de novo pedido + criado header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NovoPedidoComponent } from './pedido/novo-pedido/novo-pedido.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'pedido/novo', component: NovoPedidoComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
